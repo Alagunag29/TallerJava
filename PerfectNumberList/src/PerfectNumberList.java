db4484a4933bb64776b78e03a000d83d022b1ebb
Angel Laguna <alagunag29@gmail.com>
2016-03-02 12:37:29 -0500

    correcio en la impresion de la funcion operacion

--- PerfectNumberList/src/PerfectNumberList.java
+++ PerfectNumberList/src/PerfectNumberList.java
@@ -5,14 +5,14 @@ public class PerfectNumberList {
 
 	public static void main(String[] args) {
 		int sumPerf = 0, limite;
-		
+
 		ArrayList<Integer> listaPerf = new ArrayList();
 		ArrayList<Integer> listaDife = new ArrayList();
 		Scanner entradaTeclado = new Scanner (System.in);
-		
+
 		System.out.print("INGRESE LIMITE: ");
-		limite = entradaTeclado.nextInt(); 
-		
+		limite = entradaTeclado.nextInt();
+
 		for(int i=1;i<=limite; i++){
 			for(int j=1;j<i;j++){
 				if(i % j == 0){
@@ -26,21 +26,19 @@ public class PerfectNumberList {
 			}
 			sumPerf = 0;
 		}
-	
-        operacion(limite, listaPerf);
+
+    operacion(limite, listaPerf);
 		operacion(limite, listaDife);
 	}
-	
+
 	public static void operacion(int limite, ArrayList<Integer> lista){
 		int sumatoria = 0;
 		double promedio=0;
-		System.out.println((100*3)/1000);
 		System.out.println(lista);
 		sumatoria = lista.size();
-		promedio = ((sumatoria*100)/limite); 
-		
-		//System.out.println(sumatoria + " numeros, " + promedio + "%" );
+		promedio = ((sumatoria*100)/limite);
+		System.out.println(sumatoria + " numeros, " + promedio + "%" );
 	}
-	
-	
+
+
 }
